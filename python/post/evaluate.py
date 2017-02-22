0ac6461e02acaa1364560f9dd8eb9776b5078d66
hezhichao <hzc199307@qq.com>
2017-02-22 15:02:11 +0800

    modify evaluate.py

--- python/post/evaluate.py
+++ python/post/evaluate.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 # by hezhichao
 # 2017.02.13
-# update 2017.02.13
-# 评价模型
+# update 2017.02.22
+# 评价模型的Precision、Recall和ReviewRate
 
 import sys
 import os
@@ -15,6 +15,7 @@ class Tool:
 
 class Evaluate:
 
+	# 读取阈值文件
 	def read_thresh_file(self,thresh_file):
 		self.thresh_dict = {}
 		if os.path.exists(thresh_file) == False:
@@ -28,6 +29,7 @@ class Evaluate:
 				self.thresh_dict[int(strs[0])] = 0.0 #阈值默认为0.0
 		thresh_file_read.close()
 
+	# 读取多个阈值文件
 	def read_thresh_file_list(self,thresh_file_list):
 		self.thresh_dict_list = []
 		for thresh_file in thresh_file_list:
@@ -44,6 +46,7 @@ class Evaluate:
 					thresh_dict[int(strs[0])] = 0.0 #阈值默认为0.0
 			thresh_file_read.close()
 
+	# 读取真值文件
 	def read_groudtruth_file(self,groudtruth_file):
 		self.groudtruth_dict = {}
 		if os.path.exists(groudtruth_file) == False:
@@ -54,22 +57,20 @@ class Evaluate:
 			self.groudtruth_dict[str(strs[0])] = int(strs[1])
 		groudtruth_file_read.close()
 
+	# 评价模型的Precision、Recall
 	def process_pr(self,classify_result_file_list):
-		# 如果真值表为空，就计算Review相关的指标并且输出
-		if len(self.groudtruth_dict)==0:
-			review_tuple_list = []
-			for classify_result_file in classify_result_file_list:
-				review_tuple = Review.calculate_review(self.thresh_dict,classify_result_file)
-				review_tuple_list.append(review_tuple)
-			Review.print_review(review_tuple_list)
+		if len(self.groudtruth_dict)>0:
 		# 如果有真值表，就计算PR相关的指标并且输出
-		else:
 			pr_tuple_list = []
 			for classify_result_file in classify_result_file_list:
 				pr_tuple = PrecisionRecall.calculate_precision_recall(self.thresh_dict,self.groudtruth_dict,classify_result_file)
 				pr_tuple_list.append(pr_tuple)
 			PrecisionRecall.print_precision_recall(pr_tuple_list)
+		else:
+		# 如果真值表为空，就计算Review相关的指标并且输出
+			self.process_review(classify_result_file_list,"image")
 
+	# 评价模型的ReviewRate
 	def process_review(self,classify_result_file_list,review_type="image"):
 		if review_type == "image":
 			review_tuple_list = []
@@ -92,11 +93,12 @@ class Evaluate:
 
 class PrecisionRecall:
 
+	# 计算PR，并且返回
 	@staticmethod
 	def calculate_precision_recall(thresh_dict,groudtruth_dict,classify_result_file):
-		dict_fenzi = {}
-		dict_precision_fenmu = {}
-		dict_recall_fenmu = {}
+		dict_correct = {}
+		dict_predict_count = {}
+		dict_gt_count = {}
 		classify_result_file_read = open(classify_result_file)
 		max_cls_index = 0
 		for line in classify_result_file_read:
@@ -108,35 +110,35 @@ class PrecisionRecall:
 				cls = 0
 			gt = groudtruth_dict[strs[0]]
 			if cls == gt:
-				if dict_fenzi.has_key(cls):
-					dict_fenzi[cls] += 1
+				if dict_correct.has_key(cls):
+					dict_correct[cls] += 1
 				else:
-					dict_fenzi[cls] = 1
-			if dict_precision_fenmu.has_key(cls):
-				dict_precision_fenmu[cls] += 1
+					dict_correct[cls] = 1
+			if dict_predict_count.has_key(cls):
+				dict_predict_count[cls] += 1
 			else:
-				dict_precision_fenmu[cls] = 1
-			if dict_recall_fenmu.has_key(gt):
-				dict_recall_fenmu[gt] += 1
+				dict_predict_count[cls] = 1
+			if dict_gt_count.has_key(gt):
+				dict_gt_count[gt] += 1
 			else:
-				dict_recall_fenmu[gt] = 1
+				dict_gt_count[gt] = 1
 			if gt > max_cls_index:
 				max_cls_index = gt
 			if cls > max_cls_index:
 				max_cls_index = cls
 		classify_result_file_read.close()
-		return (classify_result_file,dict_fenzi,dict_precision_fenmu,dict_recall_fenmu,max_cls_index)
+		return (classify_result_file,dict_correct,dict_predict_count,dict_gt_count,max_cls_index)
 
 	@staticmethod
 	def print_precision_recall(pr_tuple_list):
 		for pr_tuple in pr_tuple_list:
 			classify_result_file = pr_tuple[0]
-			dict_fenzi = pr_tuple[1]
-			dict_precision_fenmu = pr_tuple[2]
-			dict_recall_fenmu = pr_tuple[3]
+			dict_correct = pr_tuple[1]
+			dict_predict_count = pr_tuple[2]
+			dict_gt_count = pr_tuple[3]
 			max_cls_index = pr_tuple[4]
-			print "# " + classify_result_file
-			print "index\tprecision\trecall\t  fenzi\tprecision_fenmu\trecall_fenmu"
+			print "PrecisionRecall " + classify_result_file
+			print "index\tprecision\trecall\tcorrect\tpredict_count\tgt_count"
 			fenzi_sum = 0
 			fenmu_sum = 0
 			fenzi_sum_1_end = 0
@@ -148,27 +150,27 @@ class PrecisionRecall:
 				recall_fenmu = 0
 				precision = "-"
 				recall = "-"
-				if dict_fenzi.has_key(cls):
-					fenzi=dict_fenzi[cls]
-				if dict_precision_fenmu.has_key(cls):
-					precision_fenmu=dict_precision_fenmu[cls]
-				if dict_recall_fenmu.has_key(cls):
-					recall_fenmu=dict_recall_fenmu[cls]
+				if dict_correct.has_key(cls):
+					fenzi=dict_correct[cls]
+				if dict_predict_count.has_key(cls):
+					precision_fenmu=dict_predict_count[cls]
+				if dict_gt_count.has_key(cls):
+					recall_fenmu=dict_gt_count[cls]
 				# if fenzi==0 and precision_fenmu==0 and recall_fenmu==0:
 				# 	continue
 				if precision_fenmu!=0:
 					precision = float(fenzi)/precision_fenmu
 				if recall_fenmu!=0:
 					recall = float(fenzi)/recall_fenmu
-				print str(cls) + "\t" + '%-10s'%('%-.5s'%str(precision)) + "\t" + '%-.5s'%str(recall) + "\t" + '%7s'%str(fenzi) + "\t" + '%15s'%str(precision_fenmu) + "\t" + '%12s'%str(recall_fenmu)
+				print str(cls) + "\t" + '%-10s'%('%-.5s'%str(precision)) + "\t" + '%-.5s'%str(recall) + "\t" + '%7s'%str(fenzi) + "\t" + '%13s'%str(precision_fenmu) + "\t" + '%12s'%str(recall_fenmu)
 				fenzi_sum += fenzi
 				fenmu_sum += precision_fenmu
 				if cls != 0:
 					fenzi_sum_1_end += fenzi
 					precision_fenmu_sum_1_end += precision_fenmu
 					recall_fenmu_sum_1_end += recall_fenmu
-			print "all\t" + '%-10s'%('%-.5s'%str(float(fenzi_sum)/fenmu_sum)) + "\t" + '%-.5s'%str(float(fenzi_sum)/fenmu_sum) + "\t" + '%7s'%str(fenzi_sum) + "\t" + '%15s'%str(fenmu_sum) + "\t" + '%12s'%str(fenmu_sum)
-			print "1-end\t" + '%-10s'%('%-.5s'%str(float(fenzi_sum_1_end)/precision_fenmu_sum_1_end)) + "\t" + '%-.5s'%str(float(fenzi_sum_1_end)/recall_fenmu_sum_1_end) + "\t" + '%7s'%str(fenzi_sum_1_end) + "\t" + '%15s'%str(precision_fenmu_sum_1_end) + "\t" + '%12s'%str(recall_fenmu_sum_1_end)
+			print "all\t" + '%-10s'%('%-.5s'%str(float(fenzi_sum)/fenmu_sum)) + "\t" + '%-.5s'%str(float(fenzi_sum)/fenmu_sum) + "\t" + '%7s'%str(fenzi_sum) + "\t" + '%13s'%str(fenmu_sum) + "\t" + '%12s'%str(fenmu_sum)
+			print "1-end\t" + '%-10s'%('%-.5s'%str(float(fenzi_sum_1_end)/precision_fenmu_sum_1_end)) + "\t" + '%-.5s'%str(float(fenzi_sum_1_end)/recall_fenmu_sum_1_end) + "\t" + '%7s'%str(fenzi_sum_1_end) + "\t" + '%13s'%str(precision_fenmu_sum_1_end) + "\t" + '%12s'%str(recall_fenmu_sum_1_end)
 			print ""
 
 
@@ -302,15 +304,17 @@ class Review:
 
 if __name__ == '__main__':
 	if(len(sys.argv)<4):
-		print "Usage 1:"
-		print "	 python evaluate.py pr <groudtruth_file< no_thresh/<thresh_file> [<classify_result_file> ...]"
-		print "Note:"
-		print "  1.阈值文件(第1列是label_index,第2列是阈值)可有可无。没有阈值的类，默认为阈值0.0；阈值为1.0的类，相当于把这个类过滤掉了。"
-		print "  2.有groundtruth文件，就计算pr指标；否则，计算复审率指标。"
+		print "Usage 1: 计算Precision与Recall"
+		print "	python evaluate.py pr <groudtruth_file> no_thresh/<thresh_file> [<classify_result_file> ...]"
+		print "	Note:"
+		print "		1.阈值文件(第1列是label_index,第2列是阈值)可有可无。没有阈值的类，默认为只要分数最大即可；阈值为1.0的类，相当于把这个类过滤掉了。"
+		print "		2.如果groundtruth文件存在，就计算pr指标；否则，计算复审率指标。"
 		print ""
-		print "Usage 2:"
-		print "	 python evaluate.py review image/video no_thresh/<thresh_file> [<classify_result_file> ...]"
-		print "	 python evaluate.py review video_combine [(no_thresh/<thresh_file> <classify_result_file>) ...]"
+		print "Usage 2: 计算ReviewRate"
+		print "	python evaluate.py review image/video no_thresh/<thresh_file> [<classify_result_file> ...]"
+		print "	python evaluate.py review video_combine [(no_thresh/<thresh_file> <classify_result_file>) ...]"
+		print "	Note:"
+		print "		1.thresh_file文件和classify_result_file文件必须成对出现，最终会得到各个文件在非0类别上的复审率之和。"
 		exit()
 	if sys.argv[1] == "pr" or sys.argv[1] == "PR":
 		groudtruth_file = sys.argv[2]
